2ef5b7fb49e816af5ba350c3f91905a0ab8899f8
Kübra Yardım <kubrayardim04@gmail.com>
2026-03-07 13:05:09 +0300

    Add files via upload

---
+++ Data Quality Control Automation/quality_check.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+# CSV yükleme
+df = pd.read_csv("titanic.csv")
+
+print("Veri Seti Boyutu:", df.shape)
+print("\nSütunlar:", df.columns)
+#df.shape → (satır sayısı, sütun sayısı) verir.
+#df.columns → sütun isimlerini listeler.
+
+# -------------------------
+# 1. Eksik veri analizi
+# -------------------------
+missing_ratio = df.isna().mean()
+#df.isna() → DataFrame’deki her hücreyi kontrol eder: NaN ise True, değilse False döner.
+#df.isna().mean() → True=1, False=0 kabul ederek her sütunun eksik veri yüzdesini hesaplar.
+
+missing_report = pd.DataFrame({
+    "Column": missing_ratio.index,
+    "MissingRatio": missing_ratio.values
+})
+#missing_ratio.index → sütun isimleri
+#missing_ratio.values → NaN oranları
+
+print("\n=== Eksik Veri Oranı ===")
+print(missing_report)
+
+
+# -------------------------
+# 2. Aykırı değer kontrolü (3 Sigma)
+# Fare sütunu üzerinden
+# -------------------------
+col = "fare"
+
+mean = df[col].mean()
+std = df[col].std()
+
+upper_limit = mean + 3*std
+lower_limit = mean - 3*std
+
+outliers = df[(df[col] > upper_limit) | (df[col] < lower_limit)]
+#Koşul: Fare değeri üst sınırdan büyük veya alt sınırdan küçükse
+# | → “veya” anlamına gelir
+#Sonuç: outliers DataFrame’i sadece aykırı değerleri içerir.
+
+print("\n=== Fare Sütununda Aykırı Değerler ===")
+print(outliers[[col]])
+
+print("\nToplam aykırı değer:", len(outliers))
+#outliers[[col]] → sadece Fare sütununu gösterir
+#len(outliers) → kaç tane aykırı değer olduğunu verir
+
+# -------------------------
+# 3. Genel Veri Kalitesi Raporu
+# -------------------------
+
+report = pd.DataFrame({
+    "Column": df.columns,
+    "MissingRatio": df.isna().mean(),
+    "DataType": df.dtypes
+})
+
+report["Outlier_Count"] = 0
+report.loc[report["Column"] == col, "Outlier_Count"] = len(outliers)
